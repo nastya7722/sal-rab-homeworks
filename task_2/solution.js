d95c078401b45f9770ea5788e760cecd0557b62a
Anastasiya Slusareva <nastya7722w@gmail.com>
2022-10-21 11:33:56 +0300

    fourth fix

--- task_2/solution.js
+++ task_2/solution.js
@@ -74,8 +74,8 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
     let freeShipping;
     // запишите без использования if или любых других условий:
     // если shippingSum равно нулю, то freeShipping должна быть равна true, иначе freeShipping должна быть равна false
-    freeShipping == shippingSum;
     shippingSum == 0;
+    freeShipping = shippingSum;
    
 
     // Конец решения задачи №2.3.
